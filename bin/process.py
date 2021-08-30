be2d070876955901c6113ed1f00a444eb081fcd2
Angelika Tyborska <angelikatyborska@fastmail.com>
2021-08-30 10:18:22 +0200

    Fix single test status is always "pass" even when tests fail (#33)
    * Add proper test case status to tests
    
    * Fix interpreting test results in process.py
    
    * Remove extra newline

--- bin/process.py
+++ bin/process.py
@@ -32,12 +32,14 @@ def process_tests_results(compilation_errors_file: str, tests_output_file: str) 
         for test in suite:
             test_data = {"name": test.name, "status": "pass"}
             # Sets test status and message in case of failure or error
-            if (t := test.result) == junitparser.Failure:
-                test_data["status"] = "fail"
-                test_data["message"] = t.message
-            elif (t := test.result) == junitparser.Error:
-                test_data["status"] = "fail"
-                test_data["message"] = t.message
+            if (len(test.result) > 0):
+                result = test.result[0]
+                if type(result) == junitparser.Failure:
+                    test_data["status"] = "fail"
+                    test_data["message"] = result.message
+                elif type(result) == junitparser.Error:
+                    test_data["status"] = "fail"
+                    test_data["message"] = result.message
 
             if out := test.system_out:
                 # Output MUST be limited at 500 chars
